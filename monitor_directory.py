e31b52e797b38d91c27144afbcc83021f06fd858
Unknown <sagsdf_github_tyler71@xyzz.work>
2018-09-08 17:40:32 -0700

    glob matching
    Removed redundant code

--- monitor_directory.py
+++ monitor_directory.py
@@ -2,11 +2,12 @@
 
 
 import argparse
+import pathlib
 import json
 import os
+import subprocess
 import tempfile
 import time
-import subprocess
 
 
 def main():
@@ -16,9 +17,24 @@ def main():
                         )
     parser.add_argument("--temp-file",
                         default=os.path.join(tempfile.gettempdir(), "monitor.tmp"),
+                        help="Location of cache file, default {}".format(tempfile.gettempdir())
                         )
-    parser.add_argument("script",
-                        nargs=1)
+    parser.add_argument("--trim-cache",
+                        action="store_true",
+                        help="Remove irrelevant directories from the cache")
+    parser.add_argument("--include",
+                        action="append",
+                        help="glob file matching, can be invoked multiple times",
+                        )
+    parser.add_argument("--exclude",
+                        action="append",
+                        help="glob file matching, can be invoked multiple times",
+                        )
+    parser.add_argument("command",
+                        nargs=1,
+                        help="Run command or script on each file: ./script file_foobar",
+                        )
+
     parser.add_argument("directories",
                         default=[os.getcwd()],
                         nargs='*',
@@ -26,10 +42,11 @@ def main():
     args = parser.parse_args()
 
     # If is file, treat it as a script. If not, run as a command
-    if os.path.isfile(os.path.abspath(args.script[0])):
-        command = os.path.abspath(args.script[0])
+    # Get scripts absolute path
+    if os.path.isfile(os.path.abspath(args.command[0])):
+        command = os.path.abspath(args.command[0])
     else:
-        command = args.script[0]
+        command = args.command[0]
 
     temp_file = args.temp_file
     args.directories = [os.path.abspath(directory) for directory in args.directories]
@@ -41,59 +58,42 @@ def main():
         with open(temp_file) as f:
             cached_directories = json.load(f)
 
-            non_existing_directories = list()
-            for directory in cached_directories:
-                if directory not in args.directories:
-                    non_existing_directories.append(directory)
-            for directory in non_existing_directories:
-                cached_directories.pop(directory)
+            if args.trim_cache is True:
+                non_existing_directories = list()
+                for directory in cached_directories:
+                    if directory not in args.directories:
+                        non_existing_directories.append(directory)
+                for directory in non_existing_directories:
+                    cached_directories.pop(directory)
     else:
         cached_directories = dict()
 
     # Decide to run as daemon or script.
     # Having updated values will trigger a write to the temporary file
-    update_json = False
     if args.daemon is True:
         while True:
             try:
-                result_cached_directories = process_files(command, cached_directories, temp_file, args.directories)
-                if result_cached_directories[0] is True:
-                    write_json_file(result_cached_directories[1], temp_file)
+                result_cached_directories = files_command_process(command,
+                                                                  cached_directories,
+                                                                  args.directories,
+                                                                  include=args.include,
+                                                                  exclude=args.exclude)
+                if result_cached_directories is True:
+                    write_json_file(cached_directories, temp_file)
                 time.sleep(1)
             except KeyboardInterrupt:
-                exit()
-            except FileNotFoundError as e:
-                print(e, "was not found, removing from tested entries")
-                # args.directories.remove()
+                break
     else:
-        result_cached_directories = process_files(command, cached_directories, temp_file, args.directories)
-        if result_cached_directories[0] is True:
-            update_json = True
-        if update_json is True:
-            write_json_file(result_cached_directories[1], temp_file)
+        result_cached_directories = files_command_process(command,
+                                                          cached_directories,
+                                                          args.directories,
+                                                          include=args.include,
+                                                          exclude=args.exclude)
+        if result_cached_directories is True:
+            write_json_file(cached_directories, temp_file)
 
 
-def process_files(command, cached_directories, temp_file, directories):
-    if os.path.isfile(temp_file):
-        result = process_cached(command, cached_directories, directories)
-    else:
-        result = process_non_cached(command, cached_directories, directories)
-    return (result, cached_directories)
-
-
-def process_non_cached(command, cache_dictionary, directories):
-    for directory in directories:
-        cache_dictionary[directory] = [os.stat(directory).st_mtime, set()]
-        directory_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-        for file in directory_files:
-            print("processing", file)
-            subprocess.run(command.split(" ") + [os.path.join(directory, file)])
-            # After processing file, cache it
-            cache_dictionary[directory][1].add(file)
-    return True
-
-
-def process_cached(command, cache_dictionary, directories):
+def files_command_process(command, cache_dictionary, directories, include=None, exclude=None):
     for directory in cache_dictionary:
         # Convert list of processed files to a set
         cache_dictionary[directory][1] = set(cache_dictionary[directory][1])
@@ -102,7 +102,10 @@ def process_cached(command, cache_dictionary, directories):
             cached_directory_time = cache_dictionary[directory][0]
             current_directory_time = os.stat(directory).st_mtime
             if current_directory_time > cached_directory_time:
-                directory_files = (f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)))
+                if include or exclude:
+                    directory_files = file_include_exclude(directory=directory, include=include, exclude=exclude)
+                else:
+                    directory_files = (f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)))
                 cache_dictionary[directory][0] = current_directory_time
                 for file in directory_files:
                     if file not in cache_dictionary[directory][1]:
@@ -112,7 +115,10 @@ def process_cached(command, cache_dictionary, directories):
                 return True
         else:
             cache_dictionary[directory] = [os.stat(directory).st_mtime, set()]
-            directory_files = (f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)))
+            if include or exclude:
+                directory_files = file_include_exclude(directory=directory, include=include, exclude=exclude)
+            else:
+                directory_files = (f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)))
             for file in directory_files:
                 print([command, os.path.join(directory, file)])
                 print("processing", file)
@@ -129,6 +135,28 @@ def write_json_file(dictionary, temp_file):
             # Convert list of processed files to a set
             cached_directories[directory][1] = list(cached_directories[directory][1])
         json.dump(cached_directories, f, indent=4, sort_keys=True)
+
+
+def file_include_exclude(*, directory, include, exclude):
+    files = [file for file in os.listdir(directory)]
+    if include:
+        included_filenames = {file for glob_match in include
+                              for file in files
+                              if pathlib.PurePath(file).match(glob_match)}
+    else:
+        included_filenames = set()
+    if exclude:
+        excluded_filenames = {file for glob_match in exclude
+                              for file in files
+                              if pathlib.PurePath(file).match(glob_match)}
+    else:
+        excluded_filenames = set()
+
+    for file in files:
+        if file in included_filenames:
+            yield file
+        elif file not in excluded_filenames and len(excluded_filenames) > 0:
+            yield file
 
 
 if __name__ == '__main__':
